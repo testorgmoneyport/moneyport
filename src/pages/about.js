044772d3bd6d9af48f2013bec1173f6b6976be68
Eda Kara <125818264+edakaraa@users.noreply.github.com>
2025-01-12 19:51:03 +0300

    About sayfası düzenlendi.

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,20 +2,20 @@ export default function About() {
     return (
       <div className="container mx-auto">
         <div
-          className="flex items-center justify-center min-h-screen bg-gray-50 bg-cover bg-center pt-20"
+          className="flex items-center justify-center min-h-screen bg-cover bg-center pt-20"
           style={{ backgroundImage: 'url(/assets/img/about-bg.png)' }}
         >
           <div className="w-full max-w-6xl flex flex-wrap">
             {/* Sol Kısım */}
             <div className="w-full md:w-1/3 flex items-center justify-center p-4">
-              <h1 className="text-4xl font-bold text-gray-900 leading-tight">
+              <h1 className="text-4xl font-bold text-gray-900 leading-tight pt-72">
                 MoneyPort ile <br /> Varlıklarınızı izleyin, <br /> yatırımlarınızı güçlendirin
               </h1>
             </div>
   
             {/* Sağ Kısım */}
             <div className="w-full md:w-2/3 px-8">
-              <p className="text-lg text-gray-700 leading-relaxed">
+              <p className="text-2xl text-gray-700 leading-relaxed pt-64">
                 Moneyport olarak, finansal özgürlük yolculuğunuzda yanınızda olmayı hedefliyoruz. 
                 Varlıklarınızı daha bilinçli yönetmenize olanak sağlayan bir platform sunarak, yatırım 
                 kararlarınızda güven ve şeffaflık sağlıyoruz. Yatırım dünyasının karmaşık yapısını 
